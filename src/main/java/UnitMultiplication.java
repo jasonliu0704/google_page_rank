f92ab6742d3c89506ca0d750542ba129912ade1d
Jason Liu <jasonliu0704@gmail.com>
2017-03-30 15:48:54 -0700

    processing and get sub page rank

--- src/main/java/UnitMultiplication.java
+++ src/main/java/UnitMultiplication.java
@@ -15,6 +15,8 @@ import java.util.List;
 
 public class UnitMultiplication {
 
+
+    // construct and process transition matrix
     public static class TransitionMapper extends Mapper<Object, Text, Text, Text> {
 
         @Override
@@ -22,9 +24,25 @@ public class UnitMultiplication {
 
             //input format: fromPage\t toPage1,toPage2,toPage3
             //target: build transition matrix unit -> fromPage\t toPage=probability
+            String line = value.toString().trim();
+            String[] fromAndTo = line.split("\t");
+
+            // dead end no outgoing edges
+            if(fromAndTo.length == 1 || fromAndTo[1].trim().equals("")){
+                return;
+            }
+
+            String from = fromAndTo[0];
+            String[] tos = fromAndTo[1].split(",");
+            for(String to: tos){
+                // output key=from, value= to=prob
+                context.write(new Text(from), new Text(to + "=" + (double)1/tos.length));
+            }
+
         }
     }
 
+    // process and construct page rank matrix
     public static class PRMapper extends Mapper<Object, Text, Text, Text> {
 
         @Override
@@ -32,9 +50,12 @@ public class UnitMultiplication {
 
             //input format: Page\t PageRank
             //target: write to reducer
+            String[] pageRank = value.toString().trim().split("\t");
+            context.write(new Text(pageRank[0]), new Text(pageRank[1]));
         }
     }
 
+    // process and calculate subPr for to page
     public static class MultiplicationReducer extends Reducer<Text, Text, Text, Text> {
 
 
@@ -44,6 +65,23 @@ public class UnitMultiplication {
 
             //input key = fromPage value=<toPage=probability..., pageRank>
             //target: get the unit multiplication
+            double pr = 0;
+            List<String> transitionUnit = new ArrayList<String>();
+            for(Text val: values){
+                if(val.toString().contains("=")){
+                    transitionUnit.add(value.toString());
+                }else{
+                    pr = Double.parseDouble(value.toString());
+                }
+            }
+
+            // multiply to get subpr
+            for(String toPage: transitionUnit){
+                String outputkey = toPage.split("=")[0];
+                double weight = Double.parseDouble(toPage.split("=")[1]);
+                String outputValue = String.valueOf(weight * pr);
+                context.write(new Text(outputkey), new Text(outputValue));
+            }
         }
     }
 
@@ -53,7 +91,9 @@ public class UnitMultiplication {
         Job job = Job.getInstance(conf);
         job.setJarByClass(UnitMultiplication.class);
 
-        //how chain two mapper classes?
+        //chain two mapper classes
+        ChainMapper.addMapper(job, TransitionMapper.class, Object.class, Text.class, Text.class, Text.class, conf);
+        ChainMapper.addMapper(job, TransitionMapper.class, Object.class, Text.class, Text.class, Text.class, conf);
 
         job.setReducerClass(MultiplicationReducer.class);
 
